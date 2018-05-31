daa17f50a7a7792292e6129ede3aaa1dd12824db
Ivan <mynamekasatkin@gmail.com>
2018-05-31 11:37:28 +0300

    just commit

--- try_2.py
+++ try_2.py
@@ -24,8 +24,7 @@ class ParserCore:
       self.dict_with_data = my_dict
 
 
-  def run_equal_data(self):
-    pass
+
 
   def csv_reader(self):
     file = open(self.file, 'r')
@@ -49,29 +48,20 @@ class ParserCore:
     for code, value in dict.items():
       l += 1
       if l <=10:
-        #print(key,value, '---------------------------------')
         url = requests.get('http://th-tool.by/index.php?route=product/search&search=' + code) # Страница с которого мы будем парсить в конце меняем на нужный нам код
         soup = BeautifulSoup(url.text, 'html.parser')
         searcher = soup.find_all('a')
-        #print(searcher)
-        #print(searcher)
         for i in searcher:
-          #print('i ' + str(i))
           if(i.get('href')):
             var = i.get('href').find(code)
-            #print(var)
             if(var >= 0):
               url = requests.get(i.get('href'))  # Страница с которого мы будем парсить в конце меняем на нужный нам код
               soup = BeautifulSoup(url.text, 'html.parser')
               try:
                 div = soup.find('div', {'class': 'image'}).next
                 href_photo = div.get('href')
-                #print('many_photo ' + str(div))
-                # print(dict_with_url)
-                # print(href_photo, code, value[1])
                 dict_with_url[href_photo] = (code, value[1])
               except AttributeError as err:
-                #print('нет такого изображение пропускаем  ' + key + value)
                 continue
     self.dict_for_download = dict_with_url
     return dict_with_url
@@ -132,50 +122,74 @@ class ParserCore:
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   def download(self, path):
     if self.dict_for_download:
       for link, data in self.dict_for_download.items():
         url = link
         print('download', link)
         get_photo = requests.get(link)
-        #format_photo = url.rsplit('.', maxsplit=1)
-        #name_photo = format_photo[0].rsplit('/')[-1]
-        #full_name = name_photo + "." + format_photo[-1]
         format_photo = url.rsplit('.', maxsplit=1)[-1]
         full_name = data[0] + "." + format_photo
         try:
          file = open(path + full_name, 'wb')
-         #file = open('./test/' + full_name, 'wb')
-         #file = open('./Yato_img/' + full_name, 'wb')
          file.write(get_photo.content)
-         #print(data, key,)
          file.close()
         except FileNotFoundError as err:
          print(err)
+
+
+
+  def dowanload_on_server(data, key, producer):
+    """
+    Функция непосредственно скачивает фото на сервере
+    """
+    if self.dict_for_download:
+    url = data
+    get_photo = requests.get(url)
+    format_photo = url.rsplit('.', maxsplit=1)[-1]
+    full_name = key + "." + format_photo
+    producer_1 = producer.capitalize()
+    """
+    Меняем имена так как они указаны с учетом из название в репозитории 
+    """
+    if producer == 'D&D' or producer == 'd&d':
+        producer = 'dandd'
+    elif producer == 'Юпитер':
+        producer = 'jupiter'
+    elif producer == 'Белстаб':
+        producer = 'belstub'
+    elif producer == 'KingTul kraft':
+        producer = 'kingtulkraft'
+    elif producer == 'big red':
+      producer = 'bigred'
+    elif producer == 'KINGTUL profi':
+        producer = 'kingtulprofi'
+    elif producer == 'ПрофКлей':
+        producer = 'profcley'
+    elif producer == 'HÖGERT technik':
+        producer = 'hogert'
+    elif producer == 'ПрофКлей':
+        producer = 'profcley' 
+
+
+     producer_2 = producer.lower()
+     producer_2 = producer_2.replace(' ', '')
+     print(producer_2 + '_bot' '/' + full_name)
+     try:
+            file = open('/var/www/opt-online/photos/bot_producer/' + producer_2 + '_bot' '/' + full_name, 'wb')
+            #file = open('./test/' + full_name, 'wb')
+            #file = open('./Yato_img/' + full_name, 'wb')
+            file.write(get_photo.content)
+            #print(data, key,)
+            file.close()
+            
+     except FileNotFoundError as err:
+            print(err)
+
+
+
+  def work_request(self):
+    pass
 
 
 
@@ -190,9 +204,9 @@ class ParserCore:
 
 
   def __cosmetic(self, brand, value):
-    self.brand = brand.lower().replace(' ', '')    
-    self.value = value.lower().replace(' ', '')
-    return self.brand, self.value             
+    brand = brand.lower().replace(' ', '')    
+    value = value.lower().replace(' ', '')
+    return brand, value             
 
 
 
@@ -208,21 +222,22 @@ class ParserCore:
 
 
 
+if __name__ == '__main__':
 
-dict_with_brands = {
-'h-d.by': ['CYNEL', 'FLO','GAV','GEKO','GERLACH','H-D','JUCO','LUND','LUXI','NORMA','POWER UP','POWERUP','PROWIN',
-       'STHOR','TOYA','VOREL','YATO','Zeta','ZMM MAXPOL','Сибиар',],
-'tools.by': ['Строительный Инструмент', 'Fermer', 'Eco', 'Startul', 'Toptul', 'Wortex', 'Solaris', 'Oleo-Mac', 'Fiskars', 
-       'Gepard', 'Юпитер', 'Starfix', 'ВОЛАТ', 'Sola', 'Wilo', 'Telwin', 'Slowik', 'Molot'],
-'th-tool.by': ['Авто Инструмент', 'BaumAuto', 'Big Red', 'BRAUMAUTO', 'D&D', 'FORCEKRAFT', 'Forsage', 'Forsage electro', 
-       'FORSAGE kids', 'HCB', 'JTC', 'KINGTUL', 'KingTul kraft', 'KINGTUL profi', 'KINGTUL(SK)', 'M7', 'MARSHAL', 'Partner', 'Prowin', 'Rotake']
-}
+  dict_with_brands = {
+  'h-d.by': ['CYNEL', 'FLO','GAV','GEKO','GERLACH','H-D','JUCO','LUND','LUXI','NORMA','POWER UP','POWERUP','PROWIN',
+         'STHOR','TOYA','VOREL','YATO','Zeta','ZMM MAXPOL','Сибиар',],
+  'tools.by': ['Строительный Инструмент', 'Fermer', 'Eco', 'Startul', 'Toptul', 'Wortex', 'Solaris', 'Oleo-Mac', 'Fiskars', 
+         'Gepard', 'Юпитер', 'Starfix', 'ВОЛАТ', 'Sola', 'Wilo', 'Telwin', 'Slowik', 'Molot'],
+  'th-tool.by': ['Авто Инструмент', 'BaumAuto', 'Big Red', 'BRAUMAUTO', 'D&D', 'FORCEKRAFT', 'Forsage', 'Forsage electro', 
+         'FORSAGE kids', 'HCB', 'JTC', 'KINGTUL', 'KingTul kraft', 'KINGTUL profi', 'KINGTUL(SK)', 'M7', 'MARSHAL', 'Partner', 'Prowin', 'Rotake']
+  }
 
 
-first  = ParserCore('OptOnline.csv', field_code=1, field_producer=2, field_article=3)
-first.add_dict(dict_with_brands)
-my_dict =  first.csv_reader()
-for key in my_dict:
-    if key == 'h-d.by':
-      first.parser_h_d(my_dict[key])
-first.download('./phot/')
+  first  = ParserCore('OptOnline.csv', field_code=1, field_producer=2, field_article=3)
+  first.add_dict(dict_with_brands)
+  my_dict =  first.csv_reader()
+  for key in my_dict:
+      if key == 'h-d.by':
+        first.parser_h_d(my_dict[key])
+  first.download('./phot/')
